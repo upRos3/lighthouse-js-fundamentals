ba24386457506f8fafd235fba209886a5689c002
J Rose <josh@upros3.com>
2018-01-03 19:06:33 +0000

    Adds conditionals exercises + arrays

---
+++ arrays-ice-cream.js
@@ -0,0 +1,20 @@
+// Do not modify these first two lines
+var iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+
+// Step 2
+
+console.log(iceCreamFlavours[0]);
+
+// Step 3
+
+console.log(iceCreamFlavours[5]);
+
+// Step 4
+
+console.log(iceCreamFlavours.length);
