0ba2c50d09e8520cc06132d4d1681d1129e15bea
J Rose <josh@upros3.com>
2018-01-04 21:38:19 +0000

    to be finished later

--- merge-arrays.js
+++ merge-arrays.js
@@ -8,7 +8,7 @@ console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
 console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
 console.log(merge([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
 
-// With my own loops
+// With my own loops (to complete later)
 
 function merge2(array3, array4) {
 
