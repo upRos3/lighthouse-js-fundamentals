3af0569f04bf36fefc8a960970e9b1e692ac2cca
J Rose <josh@upros3.com>
2018-01-05 00:51:58 +0000

    Added a while loop

--- last-index-of.js
+++ last-index-of.js
@@ -12,5 +12,25 @@ console.log(lastIndexOf([0, 1, 4, 1, 2], 2), '=?', 4);
 console.log(lastIndexOf([0, 1, 4, 1, 2], 3), '=?', -1);
 console.log(lastIndexOf([5, 5, 5], 5), '=?', 2);
 console.log(lastIndexOf([], 3), '=?', -1);
-console.log(lastIndexOf([1, 3, 3, 3, 3], 3), '=?', -1);
+console.log(lastIndexOf([1, 3, 3, 3, 3], 3), '=?', 4);
 console.log(lastIndexOf([3], 3), '=?', 0);
+
+// While loop:
+
+function lastIndexOf2(array2, value2) {
+  var i = array2.length;
+  while (i--) {
+    if (array2[i] === value2) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+console.log(lastIndexOf2([0, 1, 4, 1, 2], 1), '=?', 3);
+console.log(lastIndexOf2([0, 1, 4, 1, 2], 2), '=?', 4);
+console.log(lastIndexOf2([0, 1, 4, 1, 2], 3), '=?', -1);
+console.log(lastIndexOf2([5, 5, 5], 5), '=?', 2);
+console.log(lastIndexOf2([], 3), '=?', -1);
+console.log(lastIndexOf2([1, 3, 3, 3, 3], 3), '=?', 4);
+console.log(lastIndexOf2([3], 3), '=?', 0);
