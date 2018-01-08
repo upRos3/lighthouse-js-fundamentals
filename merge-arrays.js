12467009e9a45f0174ce99fd2ab2b376c23e64bb
J Rose <josh@upros3.com>
2018-01-08 22:42:02 +0000

    Added code without using auto functions for stretch exercise

--- merge-arrays.js
+++ merge-arrays.js
@@ -8,8 +8,24 @@ console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
 console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
 console.log(merge([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
 
-// With my own loops (to complete later)
+// Without auto functions
 
 function merge2(array3, array4) {
-
+  for (var i = 0; i < array4.length; i++) {
+   array3.push(array4[i]);
+  }
+  for (i=0; i < array3.length; i++) {
+    for (var i2 = i + 1; i2 < array3.length; i2++) {
+      if (array3[i] > array3[i2]) {
+        var tempA = array3[i];
+        array3[i] = array3[i2];
+        array3[i2] = tempA;
+      }
+    }
+  }
+  return array3;
 }
+
+console.log(merge2([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
+console.log(merge2([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
+console.log(merge2([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
